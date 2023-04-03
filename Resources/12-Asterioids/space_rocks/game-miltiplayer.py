5226cfc7eeea07893bf7f781337e5117fc8b5dc1
rugbyprof <terry.griffin@msutexas.edu>
2023-04-03 13:51:08 -0500

    ensure latest

--- Resources/12-Asterioids/space_rocks/game-miltiplayer.py
+++ Resources/12-Asterioids/space_rocks/game-miltiplayer.py
@@ -11,6 +11,92 @@ from pygame.math import Vector2
 import sys
 
 
+class GameManager:
+    """
+    - Manages all external players that are in the same game queue.
+    - Any message that gets broadcast is listened to by the game managers
+      callback method (call back is not a keyword and is named callBack to
+      fit its purpose) and handled based on its content.
+    - Commands:
+        - xy        : tells manager to put the player at the xy coords
+        - target    : tells manager to move player toward the target
+        - speed     : tells manager to change a players speed
+        - color     : tells a manager what color a dot is
+    """
+
+    def __init__(self, screen):
+        self.players = {}
+        self.screen = screen
+        self.localPlayer = None
+        self.sprites = pygame.sprite.Group()
+
+    def addPlayer(self, **kwargs):
+        """Adds a player to the local game as dictated by incoming messages."""
+        name = kwargs.get("name", None)
+        player = kwargs.get("player", None)
+        color = kwargs.get("color", None)
+        localPlayer = kwargs.get("localPlayer", False)
+
+        # we don't want to try and manage the local player instance
+        if localPlayer:
+            self.localPlayer = player.id
+        else:
+            # this is a new player that needs just a basic player class
+            # with no messaging capabilites. This is a mirror of another
+            # player somewhere else.
+            player = Spaceship(screen=self.screen, name=name, color=color)
+            self.players[name] = player
+
+    def update(self):
+        """Update all players registered with the game manager."""
+        for id, player in self.players.items():
+            player.update()
+
+    def callBack(self, ch, method, properties, body):
+        """_summary_: callback for multiple players
+
+        Args:
+            ch (pika): type of channel connection with rabbitmq
+            method (pika): async info
+            properties (pika): general info about connection
+            body (dict): only thing that really matters. This is your data
+
+        Returns:
+            dictionary: results of callback
+        """
+
+        game = method.exchange  # not used here but passed in by pika
+        exchange = method.exchange  # not used here but passed in by pika
+        body = json.loads(body.decode("utf-8"))  # where all the game commands are
+        data = body.get("data", None)
+        sender = body["sender"]
+        xy = data.get("location", None)
+        target = data.get("target", None)
+        color = data.get("color", None)
+        speed = data.get("speed", None)
+
+        if self.localPlayer != sender:
+            print(f"not local: {sender} != {self.localPlayer}")
+            if not sender in self.players:
+                self.addPlayer(name=sender, color=color)
+                print(f"Players: {len(self.players)}")
+            else:
+                if xy:
+                    self.players[sender].location.x = xy[0]
+                    self.players[sender].location.y = xy[1]
+                if target:
+                    print(f"{sender} goto to {target}")
+                    self.players[sender].goto(target[0], target[1])
+                if speed:
+                    print(f"{sender} speed to {speed}")
+                    self.players[sender].setSpeed(speed)
+                if color:
+                    print(f"{sender} color to {color}")
+                    self.players[sender].color = color
+        else:
+            print("local player")
+
+
 class SpaceRocks:
     MIN_ASTEROID_DISTANCE = 250
 
@@ -21,10 +107,8 @@ class SpaceRocks:
         self.width = 800
         self.height = 600
         self.screen = pygame.display.set_mode((self.width, self.height))
-
         # self.screen = pygame.display.set_mode((0, 0), pygame.FULLSCREEN)
         # self.background = load_sprite("space", False)
-
         self.clock = pygame.time.Clock()
         self.font = pygame.font.Font(None, 64)
         self.message = ""
